3ec420a60a1210f017ceb902f4a46c34b7ba600b
Andreas Ehrlund <a.ehrlund@gmail.com>
2019-07-28 15:26:18 +0200

    switched to mongodb for data storage

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # stats-server
 A tiny web server that collects and saves statistics on application usage.
+
+Example:
+
+``` curl -d '{"command":"get_account", "version":"1.0.5"}' -H "Content-Type: application/json" -X POST https://andli-stats-server.herokuapp.com/pymkm ```
--- main.py
+++ main.py
@@ -8,28 +8,37 @@ __author__ = "Andreas Ehrlund"
 __version__ = "0.1.0"
 __license__ = "MIT"
 
-import sys
-import csv
+import os
 from datetime import datetime
 
 from flask import Flask
 from flask import request
 from flask import jsonify
+from pymongo import MongoClient
 
 app = Flask(__name__)
-
+mdb_client = MongoClient(os.environ.get('MONGODB_URI', None))
 
 @app.route('/pymkm', methods=['POST'])
 def pymkm():
     json_data = request.get_json()
     if 'version' in json_data and 'command' in json_data:
-        data = [datetime.utcnow(), json_data['version'], json_data['command']]
+        data = {
+            'date': datetime.utcnow(),
+            'version': json_data['version'],
+            'command': json_data['command']
+        }
 
-        with open('pymkm.csv', 'a', newline='') as fd:
-            writer = csv.writer(fd)
-            writer.writerow(data)
-
-        resp = jsonify(success=True)
+        # store data row
+        try:
+            db = mdb_client.pymkm
+            collection = db.reports
+            collection.insert_one(data)
+        except Exception as err:
+            resp = jsonify(success=False)
+            print(err)
+        else:
+            resp = jsonify(success=True)
     else:
         resp = jsonify(success=False)
 
