7b6397e3ee321607420868425985dbd2cc6c10a6
Andreas Ehrlund <a.ehrlund@gmail.com>
2020-05-17 00:41:51 +0200

    geolocation data

---
+++ geolocate.py
@@ -0,0 +1,18 @@
+import requests
+
+
+def geolocate_ip(ip_addr):
+    try:
+        r = requests.get(
+            f"http://api.ipstack.com/{ip_addr}?access_key=33071ba576a6328a4ed81cf28123f088"
+        )
+        return r.json()
+    except Exception as e:
+        print(e.args)
+        return None
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    stuff = geolocate_ip("98.128.228.189")
+    print(stuff)
--- main.py
+++ main.py
@@ -11,29 +11,43 @@ __license__ = "MIT"
 import os
 from datetime import datetime, timedelta
 
+import requests
 from flask import Flask
 from flask import request
 from flask import jsonify
 from pymongo import MongoClient
 
 app = Flask(__name__)
-mdb_client = MongoClient(os.environ.get('MONGODB_URI', None), retryWrites=False)
-db_name = os.environ.get('MONGODB_URI', None).rsplit('/', 1)[-1]
+mdb_client = MongoClient(os.environ.get("MONGODB_URI", None), retryWrites=False)
+db_name = os.environ.get("MONGODB_URI", None).rsplit("/", 1)[-1]
 db = mdb_client[db_name]
 
 
-@app.route('/pymkm', methods=['GET', 'POST'])
+def geolocate_ip(ip_addr):
+    try:
+        r = requests.get(f"http://api.ipstack.com/{ip_addr}?access_key={os.environ.get("IPSTACK_API_KEY", None)}")
+        return r.json()
+    except Exception as e:
+        print(e.args)
+        return None
+
+
+@app.route("/pymkm", methods=["GET", "POST"])
 def pymkm():
-    if request.method == 'POST':
+    if request.method == "POST":
         json_data = request.get_json()
-        if 'version' in json_data and 'command' in json_data:
+        geolocation_data = geolocate_ip(request.remote_addr)
+        if "version" in json_data and "command" in json_data:
 
             data = {
-                'date': datetime.utcnow(),
-                'version': json_data['version'],
-                'uuid': json_data['uuid'],
-                'command': json_data['command'],
-                'ip': request.remote_addr
+                "date": datetime.utcnow(),
+                "version": json_data["version"],
+                "uuid": json_data["uuid"],
+                "command": json_data["command"],
+                "ip": request.remote_addr,
+                "lat": geolocation_data.latitude,
+                "long": geolocation_data.longitude,
+                "country": geolocation_data.coutry
             }
 
             # store data row
@@ -50,13 +64,15 @@ def pymkm():
             resp = jsonify(success=False)
 
         return resp
-    elif request.method == 'GET':
+    elif request.method == "GET":
         delta = timedelta(days=365)
         date_stop = datetime.now() - delta
         try:
             collection = db.reports
-            #print(f"count: {collection.count_documents({})}")
-            result = collection.find({"date": {"$gt": date_stop}}, {'_id': False}).sort("date")
+            # print(f"count: {collection.count_documents({})}")
+            result = collection.find({"date": {"$gt": date_stop}}, {"_id": False}).sort(
+                "date"
+            )
             res = list(result)
             return jsonify(res)
         except Exception as err:
